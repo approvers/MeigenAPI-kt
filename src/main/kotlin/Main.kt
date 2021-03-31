29aaecc634b92bf0e23f3065f94156e95a7fb524
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-03-31 14:18:59 +0900

    hello world

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
 fun main() {
-    print("Hello World!")
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, World!")
+            }
+        }
+    }.start(wait = true)
 }
